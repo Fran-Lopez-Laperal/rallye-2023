c2f966cf63088ac9395d68be29f24eb7ceeba40d
Francisco José López Laperal <fransanxenxo@gmail.com>
2023-05-03 16:43:12 +0200

    box-shadow cambiado

--- src/components/Documents/Documents.jsx
+++ src/components/Documents/Documents.jsx
@@ -5,64 +5,64 @@ import './Documents.css'
 const Documents = () => {
   return (
     <section className='documents'>
-        <h1>DOCUMENTACIÓN DE INTERÉS</h1>
+      <h1>DOCUMENTACIÓN DE INTERÉS</h1>
 
-        <section className='documents__section'>
-          <article className='documents__section__article'>
-            <header className='documents__section__article__header'>
+      <section className='documents__section'>
+        <article className='documents__section__article'>
+          <header className='documents__section__article__header'>
             <h2> Programa horario</h2>
-            </header>
-            <footer className='documents__section__article__footer'>
-              <button><i class="fa fa-file-pdf-o" aria-hidden="true"></i>VER</button>
-            </footer>
-          </article>
+          </header>
+          <footer className='documents__section__article__footer'>
+            <button><i class="fa fa-file-pdf-o" aria-hidden="true"></i> <p>VER</p></button>
+          </footer>
+        </article>
 
-          <article className='documents__section__article'>
-            <header className='documents__section__article__header'>
+        <article className='documents__section__article'>
+          <header className='documents__section__article__header'>
             <h2> Tablón de Anuncios</h2>
-            </header>
-            <footer className='documents__section__article__footer'>
-              <button><i class="fa fa-external-link" aria-hidden="true"></i>ACCEDER</button>
-            </footer>
-          </article>
+          </header>
+          <footer className='documents__section__article__footer'>
+            <button><i class="fa fa-external-link" aria-hidden="true"></i> <p>ACCEDER</p></button>
+          </footer>
+        </article>
 
-          <article className='documents__section__article'>
-            <header className='documents__section__article__header'>
+        <article className='documents__section__article'>
+          <header className='documents__section__article__header'>
             <h2> Cartel Prueba</h2>
-            </header>
-            <footer className='documents__section__article__footer'>
-              <button><i class="fa fa-file-pdf-o" aria-hidden="true"></i> ABRIR</button>
-            </footer>
-          </article>
+          </header>
+          <footer className='documents__section__article__footer'>
+            <button><i class="fa fa-file-pdf-o" aria-hidden="true"></i> <p>ABRIR</p></button>
+          </footer>
+        </article>
 
-          <article className='documents__section__article'>
-            <header className='documents__section__article__header'>
+        <article className='documents__section__article'>
+          <header className='documents__section__article__header'>
             <h2> Mapa General</h2>
-            </header>
-            <footer className='documents__section__article__footer'>
-              <button><i class="fa fa-file-pdf-o" aria-hidden="true"></i>ABRIR</button>
-            </footer>
-          </article>
+          </header>
+          <footer className='documents__section__article__footer'>
+            <button><i class="fa fa-file-pdf-o" aria-hidden="true"></i> <p>ABRIR</p></button>
+          </footer>
+        </article>
 
-          <article className='documents__section__article'>
-            <header className='documents__section__article__header'>
+        <article className='documents__section__article'>
+          <header className='documents__section__article__header'>
             <h2> Itinerario</h2>
-            </header>
-            <footer className='documents__section__article__footer'>
-              <button><i class="fa fa-file-pdf-o" aria-hidden="true"></i>ABRIR</button>
-            </footer>
-          </article>
+          </header>
+          <footer className='documents__section__article__footer'>
+            <button><i class="fa fa-file-pdf-o" aria-hidden="true"></i> <p>ABRIR</p></button>
+          </footer>
+        </article>
 
-          <article className='documents__section__article'>
-            <header className='documents__section__article__header'>
+        <article className='documents__section__article'>
+          <header className='documents__section__article__header'>
             <h2> Guía de Espectador</h2>
-            </header>
-            <footer className='documents__section__article__footer'>
-              <button><i class="fa fa-file-pdf-o" aria-hidden="true"></i>ABRIR</button>
-            </footer>
-          </article>
+          </header>
+          <footer className='documents__section__article__footer'>
+            <button><i class="fa fa-file-pdf-o" aria-hidden="true"></i><p>ABRIR</p></button>
+          </footer>
+        </article>
 
-        </section>
+      </section>
 
 
     </section>
