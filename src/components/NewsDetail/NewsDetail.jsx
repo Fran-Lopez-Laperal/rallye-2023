1a9208b2272b255943e9f6c0344a933f44ed7d95
Francisco José López Laperal <fransanxenxo@gmail.com>
2023-05-23 10:32:28 +0200

    footer en newsDetail

--- src/components/NewsDetail/NewsDetail.jsx
+++ src/components/NewsDetail/NewsDetail.jsx
@@ -18,11 +18,13 @@ const NewsDetail = () => {
     return (
         <article className='newsDetail'>
             <section className='newsDetail__container'>
-                <img src={itemNew.img} alt={itemNew.title} />
-                <h1>{itemNew.title}</h1>
-                <p>{itemNew.description1}</p>
-                <p>{itemNew.description2}</p>
-                <p>{itemNew.description3}</p>
+                <img className='newsDetail__container--img' src={itemNew.img} alt={itemNew.title} />
+                <footer className='newsDetail__container--footer'>
+                    <p>{itemNew.description1}</p>
+                    <p>{itemNew.description2}</p>
+                    <p>{itemNew.description3}</p>
+                </footer>
+
             </section>
 
         </article>
